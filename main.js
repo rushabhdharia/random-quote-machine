f8a7ef6938b706dd65ec75725acc17462205f371
rushabhdharia <rushabh.dharia@gmail.com>
2016-11-13 15:57:58 +0530

    Final

---
+++ main.js
@@ -0,0 +1,36 @@
+var currentQuote = "";
+var currentAuthor = "";
+
+function openURL(url){
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}
+
+function getQuote(){
+  $.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+        $('#quote-title').text(post.title);
+        $('#quote-content').html(post.content);
+        currentAuthor=post.title;
+        currentQuote=post.content;
+        // If the Source is available, use it. Otherwise hide it.
+        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+          $('#quote-source').html('Source:' + post.custom_meta.Source);
+        } 
+        else {
+          $('#quote-source').text('');
+        }
+      },
+    cache: false
+  });
+  
+}
+
+$(document).ready(function(){	
+  getQuote();
+  $('#getQuote').on("click",getQuote);
+  $('#tweet-quote').on('click', function() {
+      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+  });
+});
